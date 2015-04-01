2aa7efaeede1f20f010a0893317dd316112ed5cb
Matthew Vivian <matthew.vivian@surevine.com>
2015-04-01 20:46:21 +0100

    Created a directory watcher
    Will watch for new files in a specified directory

---
+++ src/main/java/uk/me/viv/logmyride/DirectoryWatcher.java
@@ -0,0 +1,94 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Matthew Vivian <matthew@viv.me.uk>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package uk.me.viv.logmyride;
+
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchEvent.Kind;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+/**
+ *
+ * @author Matthew Vivian <matthew@viv.me.uk>
+ */
+public class DirectoryWatcher {
+
+    private Path path;
+
+    public DirectoryWatcher(String watchDir) {
+        this.path = Paths.get(watchDir);
+    }
+
+    /**
+     *
+     * Created with help from http://andreinc.net/2013/12/06/java-7-nio-2-tutorial-writing-a-simple-filefolder-monitor-using-the-watch-service-api/
+     */
+    public void run() {
+        if (!path.toFile().isDirectory()) {
+            throw new IllegalArgumentException("Path: " + path + " is not a folder");
+        }
+
+        System.out.println("Watching path: " + path);
+
+        FileSystem fs = path.getFileSystem();
+
+        try (WatchService service = fs.newWatchService()) {
+
+            path.register(service, ENTRY_CREATE);
+
+            WatchKey key = null;
+            while (true) {
+                key = service.take();
+
+                Kind<?> kind = null;
+                for (WatchEvent<?> watchEvent : key.pollEvents()) {
+                    kind = watchEvent.kind();
+                    if (OVERFLOW == kind) {
+                        continue;
+                    } else if (ENTRY_CREATE == kind) {
+                        Path newPath = ((WatchEvent<Path>) watchEvent).context();
+                        System.out.println("New path created: " + newPath);
+                    }
+                }
+
+                if (!key.reset()) {
+                    break;
+                }
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+    }
+}
